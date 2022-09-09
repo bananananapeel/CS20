cdef1c4f9898c7247016914e14cf89faffc438ff
46221501 <46221501@L815WD220023.edu.cbe.ab.ca>
2022-09-09 10:55:14 -0600

    10:55am

---
+++ Chapter 2/src/review/TicTacToe.java
@@ -0,0 +1,47 @@
+/*
+	Program: ${BingoCard}.java          Date: ${April 6 2022}
+	Author: Anjali Sanjay
+	School: CHHS
+	Course: Computer Science 10
+	 
+	*/
+package review;
+
+public class TicTacToe 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println(""
+				+"      |     |   \r\n"
+				+"      |     |   \r\n"
+				+"      |     |   \r\n"
+				+"------------------\r\n"
+				+"      |     |   \r\n"
+				+"      |  x  |   \r\n"
+				+"      |     |   \r\n"
+				+"------------------\r\n"
+				+"      |     |   \r\n"
+				+"      |     |   \r\n"
+				+"      |     |   \r\n");//displays tictactoe sheet
+	}
+
+}
+/*
+screen dump
+
+      |     |   
+      |     |   
+      |     |   
+------------------
+      |     |   
+      |  x  |   
+      |     |   
+------------------
+      |     |   
+      |     |   
+      |     |   
+
+
+
+*/
