c76eb9a6f773989728cc13f4ce96f834832adb5b
46221501 <46221501@L815WD220023.edu.cbe.ab.ca>
2022-09-13 09:59:28 -0600

    9:59am

--- Chapter 2/src/review/CRT.java
+++ Chapter 2/src/review/CRT.java
@@ -1,3 +1,16 @@
+/*
+
+Program: CRT.java          Last Date of this Revision: September 13, 2022
+
+Purpose: Responses to critical thinking questions
+
+Author: Anjali Sanjay, 
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
+
 package review;
 
 public class CRT 
@@ -24,8 +37,8 @@ public class CRT
 		 6. package averageScore;
 		 */
 		/**
-		 7. Bytecode means using binary numbers for the computer to process while sourcecode is words, numbers,
-		 and symbols for a computer to process the code.
+		 7. Bytecode means using binary numbers for the computer to process while sourcecode is words, 
+		 numbers,and symbols for a computer to process the code.
 		 */
 		/**
 		 12. If you use print() multiple times, everything will be on the same line. While println() has the 
