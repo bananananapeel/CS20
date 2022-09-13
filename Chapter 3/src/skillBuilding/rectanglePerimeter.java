30b18cba2b7d90dc44345261d9f1a81a85acccdf
46221501 <46221501@L815WD220023.edu.cbe.ab.ca>
2022-09-13 10:25:37 -0600

    10:25am

---
+++ Chapter 3/src/skillBuilding/rectanglePerimeter.java
@@ -0,0 +1,35 @@
+/*
+
+Program: rectanglePerimeter.java          Last Date of this Revision: September 13, 2022
+
+Purpose: An application that displays numbers 1 to 8 in their binary and decimal forms
+
+Author: Anjali Sanjay, 
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
+
+package skillBuilding;
+
+import java.util.Scanner;
+
+public class rectanglePerimeter 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		
+		int W = 4;
+		
+		int L = 13;
+		
+		int P = W*2 + L*2;
+		
+		System.out.println("The Permieter of a Rectagle with a width of 4 ")
+		
+	}
+
+}
