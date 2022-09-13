379b4c4f95f0589deed8eac81113d45a5e4af872
46221501 <46221501@L815WD220023.edu.cbe.ab.ca>
2022-09-13 09:55:44 -0600

    9:55am

---
+++ Chapter 2/src/review/CRT.java
@@ -0,0 +1,36 @@
+package review;
+
+public class CRT 
+{
+
+	public static void main(String[] args) 
+	{
+		/**
+		 3. TestAverage.java
+		 	Calculates tests averages
+		 	Anjali
+		 	September 13, 2022
+		 */
+		/**
+		 4. Average.java
+		 Calculates score averages
+		 Anjali
+		 September 13, 2022
+		 */
+		/**
+		 5. package gradeCalculator;
+		 */
+		/**
+		 6. package averageScore;
+		 */
+		/**
+		 7. Bytecode means using binary numbers for the computer to process while sourcecode is words, numbers,
+		 and symbols for a computer to process the code.
+		 */
+		/**
+		 12. If you use print() multiple times, everything will be on the same line. While println() has the 
+		 stuff in it go into the next line.
+		 */
+	}
+
+}
